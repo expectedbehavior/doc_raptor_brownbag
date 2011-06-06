519854cc3fcbdf3f41a015714c870b2a6fc844bc
Joel Meador <joel@expectedbehavior.com>
2011-06-06 17:08:05 -0400

    fix pdf example

--- straight_ruby_example.rb
+++ straight_ruby_example.rb
@@ -1,7 +1,7 @@
 # Once you put your API key in below, you can run this using
 # ruby straight_ruby_example.rb
 # and two files docraptor_sample.pdf and docraptor_sample.xls will be created in the current directory
-require "apikey"
+require File.expand_path(File.dirname(__FILE__) + "/apikey.rb")
 
 class Doc
   require 'rubygems'
@@ -23,10 +23,8 @@ File.open("docraptor_sample.xls", "w+") do |f|
 end
 
 File.open("docraptor_sample.pdf", "w+") do |f|
-  File.open("docraptor_sample.xls", "w+") do |f| 
-    f.write Doc.create(:document_content => "<table name='My First Sheet'><tr><td>Cell 1</td><td>Cell 2</td></tr></table>",
-                       :name             => "docraptor_sample.xls",
-                       :document_type    => "xls",
-                       :test             => true)
-  end
+  f.write Doc.create(:document_content => "<html><body>Text in a PDF</body></html>",
+                     :name             => "docraptor_sample.pdf",
+                     :document_type    => "pdf",
+                     :test             => true)
 end
