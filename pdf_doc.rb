69592e64cd0a6798bbd17a5e1364979f42b596bf
Matt Gordon <matt@expectedbehavior.com>
2011-06-06 22:31:24 -0400

    moved helper into the folder

--- pdf_doc.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/doc.rb")
-
-html_file = ARGV[0]
-pdf_file = html_file.gsub(".html", ".pdf")
-Doc.create_brownbag_pdf(File.read(html_file), pdf_file)
-
-system("open #{pdf_file}")
